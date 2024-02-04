cc05ba6faa0d25578537ebc7fc205ec1e942ee0d
chrischriscris <18-10892@usb.ve>
2024-02-04 15:45:02 -0400

    finished

--- tarea3/ej2/perrin/matrices.go
+++ tarea3/ej2/perrin/matrices.go
@@ -1,36 +1,38 @@
 package main
 
-import ("math/big")
+import (
+	"math/big"
+)
 
 // Multiply two matrices A and B
-func MatMul(A[][]*big.Int, B[][]*big.Int) [][]*big.Int {
-    n, m, p := len(A), len(B), len(B[0])
+func MatMul(A [][]*big.Int, B [][]*big.Int) [][]*big.Int {
+	n, m, p := len(A), len(B), len(B[0])
 
-    C := make([][]*big.Int, n)
-    for i := 0; i < n; i++ {
-        C[i] = make([]*big.Int, p)
+	C := make([][]*big.Int, n)
+	for i := 0; i < n; i++ {
+		C[i] = make([]*big.Int, p)
 
-        for j := 0; j < p; j++ {
-            C[i][j] = big.NewInt(0)
-            for k := 0; k < m; k++ {
-                C[i][j].Add(C[i][j], new(big.Int).Mul(A[i][k], B[k][j]))
-            }
-        }
-    }
+		for j := 0; j < p; j++ {
+			C[i][j] = big.NewInt(0)
+			for k := 0; k < m; k++ {
+				C[i][j].Add(C[i][j], new(big.Int).Mul(A[i][k], B[k][j]))
+			}
+		}
+	}
 
-    return C
+	return C
 }
 
 // Raise matrix A to the power n using fast exponentiation
-func MatPow(A[][]*big.Int, n int) [][]*big.Int {
-    if n == 1 {
-        return A
-    }
+func MatPow(A [][]*big.Int, n int) [][]*big.Int {
+	if n == 1 {
+		return A
+	}
 
-    if n % 2 == 0 {
-        B := MatPow(A, n / 2)
-        return MatMul(B, B)
-    }
+	if n%2 == 0 {
+		B := MatPow(A, n/2)
+		return MatMul(B, B)
+	}
 
-    return MatMul(A, MatPow(A, n - 1))
+	return MatMul(A, MatPow(A, n-1))
 }
--- tarea3/ej2/perrin/perrin.go
+++ tarea3/ej2/perrin/perrin.go
@@ -1,47 +1,50 @@
 package main
 
-import ("math/big")
+import (
+	"math/big"
+)
 
+// Computes the n-th Perrin number using naive iteration
 func PerrinNaiveIter(n int) *big.Int {
-    a := big.NewInt(3)
-    b := big.NewInt(0)
-    c := big.NewInt(2)
+	a := big.NewInt(3)
+	b := big.NewInt(0)
+	c := big.NewInt(2)
 
-    for i := 3; i <= n; i++ {
-        a, b, c = b, c, new(big.Int).Add(a, b)
-    }
+	for i := 3; i <= n; i++ {
+		a, b, c = b, c, new(big.Int).Add(a, b)
+	}
 
-    return c
+	return c
 }
 
+// Computes the n-th Perrin number using fast matrix exponentiation
 func Perrin(n int) *big.Int {
-    if n == 0 {
-        return big.NewInt(3)
-    }
+	if n == 0 {
+		return big.NewInt(3)
+	}
 
-    if n == 1 {
-        return big.NewInt(0)
-    }
+	if n == 1 {
+		return big.NewInt(0)
+	}
 
-    if n == 2 {
-        return big.NewInt(2)
-    }
+	if n == 2 {
+		return big.NewInt(2)
+	}
 
-    // P = |0 1 1|
-    //     |1 0 0|
-    //     |0 1 0|
-    P := make([][]*big.Int, 3)
-    P[0] = []*big.Int{big.NewInt(0), big.NewInt(1), big.NewInt(1)}
-    P[1] = []*big.Int{big.NewInt(1), big.NewInt(0), big.NewInt(0)}
-    P[2] = []*big.Int{big.NewInt(0), big.NewInt(1), big.NewInt(0)}
+	// P = |0 1 1|
+	//     |1 0 0|
+	//     |0 1 0|
+	P := make([][]*big.Int, 3)
+	P[0] = []*big.Int{big.NewInt(0), big.NewInt(1), big.NewInt(1)}
+	P[1] = []*big.Int{big.NewInt(1), big.NewInt(0), big.NewInt(0)}
+	P[2] = []*big.Int{big.NewInt(0), big.NewInt(1), big.NewInt(0)}
 
-    // P^(n-2)
-    power := MatPow(P, n - 2)
+	// P^(n-2)
+	power := MatPow(P, n-2)
 
-    // c = 2P[0][0] + 3P[0][2]
-    c := new(big.Int).Mul(power[0][0], big.NewInt(2))
-    c.Add(c, new(big.Int).Mul(power[0][2], big.NewInt(3)))
+	// c = 2P[0][0] + 3P[0][2]
+	c := new(big.Int).Mul(power[0][0], big.NewInt(2))
+	c.Add(c, new(big.Int).Mul(power[0][2], big.NewInt(3)))
 
-    return c
+	return c
 }
-
