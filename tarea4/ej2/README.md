c38ac1910f7ad256cba853fce24bebfb078d8863
chrischriscris <18-10892@usb.ve>
2024-02-10 23:30:46 -0400

    un readme de goodarrays

--- tarea4/ej2/README.md
+++ tarea4/ej2/README.md
@@ -1,18 +1,14 @@
 # Ejercicio 1
 
-Para compilar el programa se debe ejecutar el siguiente comando:
-
-```bash
-comando1
-```
-
 Para ejecutar el programa se debe ejecutar el siguiente comando:
 
 ```bash
-comando2
+./good-arrays <a1> <a2> ... <an>
 ```
 
-La función específica que resuelve el problema es `función` en el archivo [ruta/archivo](ruta/archivo).
+Donde `a1`, `a2`, ..., `an` son los elementos del arreglo.
+
+La función específica que resuelve el problema es `good_arrays` en el archivo [good-arrays.sh](good-arrays.sh).
 
 ---
 Hecho con <3 por chrischriscris
--- tarea4/ej2/good-arrays.sh
+++ tarea4/ej2/good-arrays.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
-# Returns the number of good arrays of the passed array
+# Returns the number of good sub-arrays of the passed array
 function good_arrays {
     A=("$@")
     local n=${#A[@]}
 
-    C0=( $(for i in $(seq $n); do echo 1; done) )
-    C1=( $(for i in $(seq $n); do echo 0; done) )
-
+    C1=( $(for i in $(seq $n); do echo 1; done) )
     local result=$n
-    for j in $(seq 2 $(($n-1))); do
+
+    for j in $(seq 2 $n); do
+        local C0=( "${C1[@]}" )
+        C1=( $(for i in $(seq $n); do echo 0; done) )
+
         local acc=0
         for i in $(seq 0 $(($n-1))); do
             if [ $i -ge $(($j-1)) ] && [ $((${A[$i]} % $j)) -eq 0 ]; then
@@ -19,8 +21,6 @@ function good_arrays {
             acc=$(($acc+${C0[$i]}))
         done
 
-        C0=( "${C1[@]}" )
-        C1=( $(for i in $(seq $n); do echo 0; done) )
     done
 
     return $result
