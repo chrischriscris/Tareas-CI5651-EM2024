d73d9bec2af84b9fc40241309c9f926ef9319f07
chrischriscris <18-10892@usb.ve>
2024-02-12 14:29:43 -0400

    trying

--- tarea4/ej4/src/main.jl
+++ tarea4/ej4/src/main.jl
@@ -17,11 +17,36 @@ function ryanair(M)
     return ryanairDP(0, M, p, n)
 end
 
-function ryanairDP(c, M, p, n)
-    println("c: ", c)
-    println("p: ", p)
-    println("n: ", n)
 
+# parejaDP(C: Conjunto, d: funcion, p: Arreglo):
+#     if C==M:
+#         return p[s]
+#
+#     if p[C] != -1:
+#         return p[C]
+#
+#     min = inf
+#     for el_i in M-C:
+#         for el_j in M-S:
+#             dist = d(0, el_i) + d(el_i, el_j) + d(el_j, 0)
+#             if dist < min:
+#                 min_el_j = el_j
+#                 min = dist
+#
+#    p[C] = parejaDP(C U {el_i, el_j}, d, p) + mejor
+#    return p[C]
+
+function contains(c, i)
+    return (c & (1 << (i - 1))) != 0
+end
+
+function dist(A, B)
+    x1, y1 = A
+    x2, y2 = B
+    return (x2 - x1)^2 + (y2 - y1)^2
+end
+
+function ryanairDP(c, M, p, n)
     if c == (1 << n) - 1
         return 0
     end
@@ -30,33 +55,27 @@ function ryanairDP(c, M, p, n)
         return p[c+1]
     end
 
-    for i in 1:n
-        if (c & (1 << i)) == 0
-            xi, yi = M[i]
-            base = xi^2 + yi^2
-            min = 2 * base
-            best_j = 0
-            for j in i+1:n
-                if (c & (1 << j)) == 0
-                    xj, yj = M[j]
-                    dist = base + xj^2 + yj^2 + (xi - xj)^2 + (yi - yj)^2
-                    if dist < min
-                        min = dist
-                        best_j = j
+    min = Inf64
+    min_A = 0
+    min_B = 0
+    O = (0, 0)
+    for A in 1:n
+        if !contains(c, A)
+            for B in 1:n
+                if !contains(c, B)
+                    distOABO = dist(O, M[A]) + dist(M[A], M[B]) + dist(M[B], O)
+                    if distOABO < min
+                        min = distOABO
+                        min_A = A
+                        min_B = B
                     end
                 end
             end
-
-            if best_j == 0
-                p[c+1] = ryanairDP(c | (1 << i), M, p, n) + min
-            else
-                p[c+1] = ryanairDP(c | (1 << i) | (1 << best_j), M, p, n) + min
-            end
-
-            return p[c+1]
         end
     end
 
+    p[c+1] = ryanairDP(c | (1 << (min_A-1)) | (1 << (min_B-1)), M, p, n) + min
+    return p[c+1]
 end
 
 function main()
@@ -68,9 +87,7 @@ function main()
     filename = ARGS[1]
     M = parseinstance(filename)
 
-    println(M)
-
-    ryanair(M)
+    print(ryanair(M))
 end
 
 main()
