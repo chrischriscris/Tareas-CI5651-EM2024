5396771d3984c8485792cf09773f80f74dc0dd6d
Christopher Gómez <18-10892@usb.ve>
2024-03-05 22:47:36 -0400

    avances duros

--- tarea6/ej2/src/main.swift
+++ tarea6/ej2/src/main.swift
@@ -1,4 +1,11 @@
-// ========== TREE ==========
+/**
+ * Node class for a tree.
+ * It has a value, a count of nodes in the subtree rooted at the current node,
+ * a depth, a list of children, a parent, a chain number and a chain index.
+ *
+ * The chain number is the index of the chain in the heavy-light decomposition.
+ * The chain index is the index of the node in the chain.
+ */
 class Node<T>: CustomStringConvertible {
     var value: T;
     var count: Int = 1;
@@ -52,12 +59,6 @@ func dfs<T>(node: Node<T>, depth: Int) {
     }
 }
 
-/**
- * Heavy-Light Decomposition implementation.
- * Takes a tree and a list, and fills the list with the heavy paths of the tree.
- *
- * First call should be with isFirst = true and chainNumber = 0.
- */
 func hld<T>(_ tree: Node<T>, _ chains: inout [[T]], _ isFirst: Bool) {
     if isFirst {
         chains.append([tree.value]);
@@ -88,26 +89,145 @@ func hld<T>(_ tree: Node<T>, _ chains: inout [[T]], _ isFirst: Bool) {
     }
 }
 
-func heavyLightDecomposition<T>(_ tree: Node<T>) {
+/**
+ * Heavy-Light Decomposition implementation.
+ * Takes a tree and a list, and fills the list with the heavy paths of the tree.
+ */
+func heavyLightDecomposition<T>(_ tree: Node<T>) -> [[T]] {
     dfs(node: tree, depth: 0);
 
     var chains: [[T]] = [];
     hld(tree, &chains, true);
 
-    print(chains);
+    return chains;
+}
+
+// ========== TREE ==========
+/**
+ * Segment tree for a list of booleans.
+ * Supports queries for existence and all in a given range.
+ */
+class BoolSegmentTree: CustomStringConvertible {
+    var left: BoolSegmentTree?;
+    var leftBound: Int = 0;
+
+    var right: BoolSegmentTree?;
+    var rightBound: Int = 0;
+
+    var value: [String: Bool];
+
+    init(_ arr: [Bool], _ _leftBound: Int, _ _rightBound: Int) {
+        leftBound = _leftBound;
+        rightBound = _rightBound;
+        let n = arr.count;
+
+        if n == 0 {
+            value = ["exists": false, "all": true];
+            return;
+        }
+
+        if n == 1 {
+            value = ["exists": arr.first!, "all": arr.first!];
+            return;
+        }
+
+        let mid = n / 2;
+        left = BoolSegmentTree(
+            Array(arr[0..<mid]),
+            leftBound,
+            leftBound + mid - 1
+        );
+        right = BoolSegmentTree(
+            Array(arr[mid..<n]),
+            leftBound + mid,
+            rightBound
+        );
+
+        value = [
+            "exists": left!.value["exists"]! || right!.value["exists"]!,
+            "all": left!.value["all"]! && right!.value["all"]!
+        ];
+    }
+
+    var description: String {
+        var str = "Value: \(value)";
+        if left != nil {
+            str += "\nLeft: \(left!.description)";
+        }
+
+        if right != nil {
+            str += "\nRight: \(right!.description)";
+        }
+
+        return str;
+    }
+
+    func query(_ i: Int, _ j: Int) -> [String: Bool] {
+        if i > rightBound || j < leftBound {
+            return ["exists": false, "all": true];
+        }
+
+        if i <= leftBound && rightBound <= j {
+            return value;
+        }
+
+        let leftQuery = left!.query(i, j);
+        let rightQuery = right!.query(i, j);
+
+        return [
+            "exists": leftQuery["exists"]! || rightQuery["exists"]!,
+            "all": leftQuery["all"]! && rightQuery["all"]!
+        ];
+    }
+
+    func exists(_ i: Int, _ j: Int) -> Bool {
+        return query(i, j)["exists"]!;
+    }
+
+    func all(_ i: Int, _ j: Int) -> Bool {
+        return query(i, j)["all"]!;
+    }
+}
+
+/**
+ * Builds a segment tree for a list of elements, given a predicate.
+ * The predicate is used to determine the value of the edges in the tree.
+ */
+func buildSegmentTree<T>(
+    _ chain: [T],
+    _ predicate: (T, T) -> Bool
+) -> BoolSegmentTree {
+    var i = 0;
+    var edges: [Bool] = [];
+    while (i < chain.count - 1) {
+        edges.append(predicate(chain[i], chain[i + 1]));
+        i += 1;
+    }
+
+    return BoolSegmentTree(edges, 0, edges.count - 1);
 }
 
 // ========== MAIN ==========
 
-let nodes: [Node<Int>] = (1...11).map { Node(value: $0) };
+func main() {
+    let nodes: [Node<Int>] = (1...11).map { Node(value: $0) };
 
-let tree = nodes[0];
-tree.add(children: Array(nodes[1...3]));
-nodes[1].add(children: Array(nodes[4...5]));
-nodes[2].add(child: nodes[6]);
-nodes[5].add(child: nodes[7]);
-nodes[6].add(child: nodes[8]);
-nodes[7].add(children: Array(nodes[9...10]));
+    let tree = nodes[0];
+    tree.add(children: Array(nodes[1...3]));
+    nodes[1].add(children: Array(nodes[4...5]));
+    nodes[2].add(child: nodes[6]);
+    nodes[5].add(child: nodes[7]);
+    nodes[6].add(child: nodes[8]);
+    nodes[7].add(children: Array(nodes[9...10]));
 
-heavyLightDecomposition(tree)
+    func predicate(parent a: Int, child b: Int) -> Bool {
+        return [1, 2, 9].contains(a);
+    }
 
+    var segmentTrees: [BoolSegmentTree] = [];
+    for chain in heavyLightDecomposition(tree) {
+        segmentTrees.append(buildSegmentTree(chain, predicate));
+    }
+}
+
+main();
